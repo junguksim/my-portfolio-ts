93890a24aaebf72d96a30a2b334694bd8d0257ba
SimJungUk <simju9397@gmail.com>
2021-01-04 19:51:06 +0900

    feat : hexagon 이미지

--- src/About/StrongPointBox.component.tsx
+++ src/About/StrongPointBox.component.tsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./StrongPointBox.css";
 
 function StrongPointBox() {
-  return <div className="StrongPointBox"></div>;
+  return (
+    <div className="StrongPointBox">
+      <div className="hexagon">
+        <div className="hexagon-in1">
+          <div className="hexagon-in2"></div>
+        </div>
+      </div>
+      <p className="strong-point-heading">꾸준함</p>
+      <span className="strong-point-content">나는 꾸준하다구</span>
+    </div>
+  );
 }
 
 export default StrongPointBox;
